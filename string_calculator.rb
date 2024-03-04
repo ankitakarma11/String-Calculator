2e89d6703a5ad7e42bdb754ae6af13aae23e4586
ankita k <ankitakarma981@gmail.com>
2024-03-04 16:51:12 +0530

    Refactor code and updated it for handle and  raise exception for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,14 +1,24 @@
 class StringCalculator
   def self.add(numbers)
     sum = 0
+    negatives = []
+
     return sum if numbers.empty?
 
     numbers = numbers.split(",")
     numbers.each do |num|
       num = num.to_i
-      sum = sum + num
+      if num.negative?
+        negatives << num 
+      else
+        sum = sum + num
+      end
     end
 
-    sum
+    if negatives.size > 1
+      raise "Negative numbers not allowed: #{negatives.join(',')}"
+    else
+      return sum
+    end
   end
 end
