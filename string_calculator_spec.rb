e5c4cae479a6c67e746e4d99fc5d44f3cf0e7092
ankita k <ankitakarma981@gmail.com>
2024-03-04 16:29:38 +0530

    simplest test case of an empty string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,4 +1,9 @@
 require_relative 'string_calculator'
 
 describe StringCalculator do
+   context "when given an empty string" do
+    it "returns 0" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+  end
 end
