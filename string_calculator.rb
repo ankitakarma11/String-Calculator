f797717b472a58c070db4ed8c90b5286a3b11922
ankita k <ankitakarma981@gmail.com>
2024-03-04 16:41:11 +0530

    updated .add method to handle an any amount of numbers seperated by comma

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,14 @@
 class StringCalculator
   def self.add(numbers)
-    return 0 if numbers.empty?
+    sum = 0
+    return sum if numbers.empty?
+
+    numbers = numbers.split(",")
+    numbers.each do |num|
+      num = num.to_i
+      sum = sum + num
+    end
+
+    sum
   end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,9 +1,21 @@
 require_relative 'string_calculator'
 
 describe StringCalculator do
-   context "when given an empty string" do
+  context "when given an empty string" do
     it "returns 0" do
       expect(StringCalculator.add("")).to eq(0)
+    end
+  end
+
+  context "when given a single number" do
+    it "returns the number" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+  end
+
+  context "when given two numbers seperated by comma" do
+    it "returns the sum of the numbers" do
+      expect(StringCalculator.add("3, 2")).to eq(5)
     end
   end
 end
